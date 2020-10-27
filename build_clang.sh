037b4f094b1a8459ed68c4bbb86ac4f6e03cda94
dakkshesh07 <dakkshesh5@gmail.com>
2020-10-28 00:13:16 +0530

    Added support and build.sh for clang compilation
    if you want to compile with gcc then use build_gcc.sh
    
    if you want to compile with clang then use build_clang.sh

---
+++ build_clang.sh
@@ -0,0 +1,72 @@
+clear
+
+git clone https://github.com/stormbreaker-project/aarch64-linux-android-4.9.git 64bit
+
+git clone https://github.com/stormbreaker-project/arm-linux-androideabi-4.9.git 32bit
+
+git clone https://github.com/kdrag0n/proton-clang.git clang
+
+ANYKERNEL3_DIR=$PWD/anykernel/
+
+GREEN="\033[01;32m"
+RST="\033[0m"
+
+RED="\033[01;32m"
+NC="\033[0m"
+
+echo "${GREEN}cleaning old files${RST}"
+
+echo "**** Removing leftovers ****"
+rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
+rm -rf $ANYKERNEL3_DIR/dtbo.img
+rm -rf $ANYKERNEL3_DIR/DuskMane_Kernel.zip
+
+cd compiler-tools
+cp -R anykernel $cd ..
+cd ..
+
+rm -rf out
+
+make clean && make distclean && make mrproper
+
+mkdir -p out
+
+
+
+echo "${GREEN}copying needed tools from compiler-tools${RST}"
+
+echo "${GREEN}starting build now, go have a cup coffee or tea${RST}"
+
+sleep 5
+
+make O=out ARCH=arm64 duskmane_defconfig
+
+PATH="${PWD}/clang/bin:${PWD}/64bit/bin:${PWD}/32bit/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+
+
+	if [ -f out/arch/arm64/boot/Image.gz-dtb ]; then
+
+		cp out/arch/arm64/boot/Image.gz-dtb anykernel
+    cd anykernel
+    	
+    sudo zip -r9 DuskMane_Kernel.zip .
+
+
+    cd ..
+
+    echo "${GREEN}zip file made!${RST}"
+    echo "${GREEN}check your zipped file in anykernel folder!${RST}"
+    echo "${GREEN}finished...... bye!${RST}"
+
+    else
+    	echo "${RED}build failed${NC}"
+    fi
+
+echo "script ended"
